90708dc58aeb7816314bbb1b4ab90bbb51ec43bd
theodesp <theo.despoudis@wpengine.com>
2021-04-22 20:59:53 +0100

    Updates

--- src/main/java/CeasarCipher.java
+++ src/main/java/CeasarCipher.java
@@ -1,14 +1,18 @@
 public class CeasarCipher {
     // Ceasar Cipher
-    String decode(String code) {
+    static String encode(String code) {
         StringBuilder sb = new StringBuilder();
         var lastCharValue = 'z';
         var alphabetLength = 'z' - 'a' + 1;
         for (char character: code.toCharArray()) {
-            var charNoRotation = character + 5;
+            var charNoRotation = character + 13;
             var charValue = charNoRotation < lastCharValue ? charNoRotation : charNoRotation - alphabetLength;
            sb.append((char)charValue);
         }
         return sb.toString();
+    }
+
+    public static void main(String[]args) {
+        System.out.println(encode("ATTACKATONCE"));
     }
 }
---
+++ src/main/java/StringPerms.java
@@ -0,0 +1,32 @@
+
+public class StringPerms {
+    static void permute(String str, int l, int r)
+    {
+        if (l == r)
+            System.out.println(str);
+        else
+        {
+            for (int i = l; i <= r; i++)
+            {
+                str = swap(str,l,i);
+                permute(str, l+1, r);
+                str = swap(str,l,i);
+            }
+        }
+    }
+
+
+    static String swap(String a, int i, int j)
+    {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i] ;
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+
+    public static void main(String[] args) {
+        permute("ARWFG", 0, 4);
+    }
+}
