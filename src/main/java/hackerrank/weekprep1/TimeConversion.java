69e0e9f9d352b0d893e7309d4281cc48510590e6
theodesp <328805+theodesp@users.noreply.github.com>
2022-08-23 16:54:09 +0100

    Add TimeConversion
    Signed-off-by: theodesp <328805+theodesp@users.noreply.github.com>

---
+++ src/main/java/hackerrank/weekprep1/TimeConversion.java
@@ -0,0 +1,53 @@
+package hackerrank.weekprep1;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+ */
+public class TimeConversion {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = bufferedReader.readLine();
+
+        String result = timeConversion(s);
+
+        System.out.println(result);
+
+        bufferedReader.close();
+
+    }
+
+    public static String timeConversion(String s) {
+        var amOrPm = s.substring(s.length() - 2);
+        var hours = s.substring(0, 2);
+        var minutes = s.substring(3, 5);
+        var seconds = s.substring(6, 8);
+        if (amOrPm.equals("PM")) {
+            var h = Integer.parseInt(hours);
+            if(h != 12)
+            hours = String.valueOf(h + 12);
+    
+        } else {
+            var h = Integer.parseInt(hours);
+            if (h == 12) {
+                hours = "00";
+            }
+        }
+        return String.format("%s:%s:%s", hours, minutes, seconds);
+    }
+}
