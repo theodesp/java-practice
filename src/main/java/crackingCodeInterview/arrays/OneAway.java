35b8bbcb5ac73fd9cf95528191d3d4e4cfb20517
theodesp <328805+theodesp@users.noreply.github.com>
2022-09-05 18:46:18 +0100

    CrackinCode Interview Arrays PT2

--- src/main/java/crackingCodeInterview/arrays/OneAway.java
+++ src/main/java/crackingCodeInterview/arrays/OneAway.java
@@ -1,13 +1,67 @@
 package crackingCodeInterview.arrays;
 
 /**
- * There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
+ * There are three types of edits that can be performed on strings: insert a
+ * character, remove a character, or replace a character. Given two strings,
+ * write a function to check if they are one edit (or zero edits) away.
  * 
- * pale, ple -> true 
- * pales, pale -> true 
- * pale, bale -> true 
+ * pale, ple -> true
+ * pales, pale -> true
+ * pale, bale -> true
  * pale, bake -> false
  */
 public class OneAway {
-    
+    public static void main(String[] args) {
+        System.out.println(isOneAway("pale", "ple"));
+        System.out.println(isOneAway("pales", "pale"));
+        System.out.println(isOneAway("pale", "bale"));
+        System.out.println(isOneAway("pale", "bake"));
+        System.out.println(isOneAway("pale", "palezz"));
+    }
+
+    static boolean isOneAway(String s1, String s2) {
+        // true if strings are same
+        if (s1.equals(s2)) {
+            return true;
+        }
+        if (Math.abs(s1.length() - s2.length()) > 1)
+            return false;
+        int i = 0, j = 0;
+        int count = 0;
+        while (i < s1.length() && j < s2.length()) {
+
+            // If current characters don't match
+            if (s1.charAt(i) != s2.charAt(j)) {
+                if (count == 1)
+                    return false;
+
+                // If length of one string is
+                // more, then only possible edit
+                // is to remove a character
+                if (s1.length() > s2.length())
+                    i++;
+                else if (s1.length() < s2.length())
+                    j++;
+                else // If lengths of both strings
+                     // is same
+                {
+                    i++;
+                    j++;
+                }
+
+                // Increment count of edits
+                count++;
+            } else {
+                i++;
+                j++;
+            }
+        }
+
+        // If last character is extra
+        // in any string
+        if (i < s1.length() || j < s2.length())
+            count++;
+
+        return count == 1;
+    }
 }
--- src/main/java/crackingCodeInterview/arrays/StringCompression.java
+++ src/main/java/crackingCodeInterview/arrays/StringCompression.java
@@ -1,8 +1,43 @@
 package crackingCodeInterview.arrays;
 
 /**
- * Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed"string would not become smaller than the original string, your method shouId return the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+ * Implement a method to perform basic string compression using the counts of
+ * repeated characters.
+ * For example, the string aabcccccaaa would become a2blc5a3. If the
+ * "compressed"string would not become smaller than the original string, your
+ * method shouId return the original string. You can assume the string has only
+ * uppercase and lowercase letters (a - z).
  */
 public class StringCompression {
-    
+    public static void main(String[] args) {
+        System.out.println(encode("aabcccccaaa"));
+        System.out.println(encode("hfqjedfffkkqjffmvvvvxzaaqqq"));
+
+    }
+
+    static String encode(String s) {
+        // stores output string
+        var encoding = new StringBuilder();
+
+        // base case
+        if (s == null) {
+            return encoding.toString();
+        }
+
+        int count;
+
+        for (int i = 0; i < s.length(); i++) {
+            // count occurrences of character at index `i`
+            count = 1;
+            while (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+
+            // append current character and its count to the result
+            encoding.append(String.valueOf(count) + s.charAt(i));
+        }
+
+        return encoding.toString();
+    }
 }
