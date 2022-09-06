19a0d4591fcf4a3744f6ae1c0b0be25673f3e64d
theodesp <328805+theodesp@users.noreply.github.com>
2022-09-06 16:04:06 +0100

    ZeroMatrix

--- src/main/java/crackingCodeInterview/arrays/RotateMatrix.java
+++ src/main/java/crackingCodeInterview/arrays/RotateMatrix.java
@@ -1,8 +1,45 @@
 package crackingCodeInterview.arrays;
 
+import java.util.Arrays;
+
 /**
- * Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+ * Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, 
+ * write a method to rotate the image by 90 degrees. Can you do this in place?
+ * 
+ * Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. 
+ * Can you do this in place?
  */
 public class RotateMatrix {
-    
+    public static void main(String[] args) {
+        var image = new int[][]{
+            new int[]{ 1, 2, 3, 4},
+            new int[]{ 5, 6, 7, 8},
+            new int[]{ 9,10,11,12},
+            new int[]{ 13,14,15,16},
+        };
+
+        for (var line: rotate(image)) {
+            System.out.println(Arrays.toString(line));
+        }
+    }
+
+    static int [][] rotate(int [][] image) {
+        var N = image.length;
+
+        for (var i = 0; i < image.length / 2; i++) {
+            for (int j = i; j < N - i - 1; j++) {
+                // Save current element
+                var temp = image[i][j];
+                // rotate current with bottom left
+                image[i][j] = image[N - i - 1][i];
+                // rotate bottom left with bottom right
+                image[N - i - 1][i] = image[N - i - 1][N - j - 1];
+                // rotate bottom right with top right
+                image[N - i - 1][N - j - 1] = image[j][N - i - 1];
+                // rotate top right with temp
+                image[j][N - 1 - i] = temp;
+            }
+        }
+        return image;
+    }
 }
--- src/main/java/crackingCodeInterview/arrays/ZeroMatrix.java
+++ src/main/java/crackingCodeInterview/arrays/ZeroMatrix.java
@@ -1,8 +1,47 @@
 package crackingCodeInterview.arrays;
 
+import java.util.*;
+
 /**
- * Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
+ * Write an algorithm such that if an element in an MxN matrix is 0, 
+ * its entire row and column are set to 0.
+ * https://www.geeksforgeeks.org/a-boolean-matrix-question/
  */
 public class ZeroMatrix {
-    
+    public static void main(String[] args) {
+        var image = new int[][]{
+            new int[]{ 1, 2, 3, 4},
+            new int[]{ 5, 0, 7, 8},
+            new int[]{ 9,10,11,12},
+            new int[]{ 13,14,15,16},
+        };
+
+        for (var line: zero(image)) {
+            System.out.println(Arrays.toString(line));
+        }
+    }
+    static int [][] zero(int [][] matrix) {
+        var columns = new HashMap<Integer, Boolean>();
+        for (var i=0;i<matrix.length;i++) {
+            var flag = false;
+            for (var j=0;j<matrix[i].length;j++) {
+                if (matrix[i][j] == 0) {
+                    flag=true;
+                    columns.put(j, true);
+                }
+            }
+            if (flag == true) {
+                for (var j=0;j<matrix[i].length;j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (var j: columns.keySet()) {
+            for (var i=0; i<matrix.length;i++) {
+                matrix[i][j] = 0;
+            }
+        }
+        return matrix;
+    }
 }
